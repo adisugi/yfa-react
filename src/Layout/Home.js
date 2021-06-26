2a07596edcbb97fbc91f28395bc88ed11ef0ccb7
adisugiarto <adisugiarto270@gmail.com>
2021-06-26 12:49:20 +0700

    menu home

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -9,6 +9,7 @@ import bg3 from '../img/5.jpg'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 import '../Style/Home.scss'
+import {Link} from "react-router-dom";
 // import {carousel} from "bootstrap-4-react/src";
 
 class Home extends Component {
@@ -24,9 +25,31 @@ class Home extends Component {
                 <div>
                     <Carousel w="100" id="carouselExampleControls">
                         <Carousel.Inner>
-                            <Carousel.Item active className="carousel-item carousel-item-1"><BImg display="block" w="100" src={bg} /></Carousel.Item>
-                            <Carousel.Item className="carousel-item carousel-item-2"><BImg display="block" w="100" src={bg2} /></Carousel.Item>
-                            <Carousel.Item className="carousel-item carousel-item-3"><BImg display="block" w="100" src={bg3} /></Carousel.Item>
+                            {/*style={{backgroundImage: `url(${bg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}*/}
+                            <Carousel.Item active className="carousel-item carousel-item-1">
+                                <BImg display="block" w="100" src={bg} />
+                                <div className="carousel-caption">
+                                    <h5>Express Your <br/>Online Bussines</h5>
+                                    <p>Jasa pengiriman berbasis teknologi</p>
+                                </div>
+                            </Carousel.Item>
+                            <Carousel.Item className="carousel-item carousel-item-2">
+                                <BImg display="block" w="100" src={bg2} />
+                                <div className="carousel-caption">
+                                    <h5>YFA Express tetap <br/> layani pengiriman <br/> tanpa libur</h5>
+                                    <p>lihat layanan</p>
+                                    <Link to={"/layanan"}>
+                                        <button className="btn btn-primary">Klik disini</button>
+                                    </Link>
+                                </div>
+                            </Carousel.Item>
+                            <Carousel.Item className="carousel-item carousel-item-3">
+                                <BImg display="block" w="100" src={bg3} />
+                                <div className="carousel-caption">
+                                    <h5>INOVASI TANPA HENTI</h5>
+                                    <p>Bersama kami #LebihDekatLebihMudah</p>
+                                </div>
+                            </Carousel.Item>
                         </Carousel.Inner>
                         <Carousel.Prev href="#carouselExampleControls">
                             <i className="fa-chevron-left" ><FontAwesomeIcon icon={faChevronLeft}/></i>
