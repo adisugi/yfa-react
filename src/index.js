c44982cfa6f087ee4ad03f0cebe326b6aeaed69c
adisugiarto <adisugiarto270@gmail.com>
2021-06-28 20:08:39 +0700

    intercept axios

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import './axiosintercept'
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </HashRouter>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
